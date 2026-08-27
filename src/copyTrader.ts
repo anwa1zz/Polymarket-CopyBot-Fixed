7db3dcccf6f37dea31f0e2ed97e377164409aed5
anwa1zz <163604536+anwa1zz@users.noreply.github.com>
2026-08-27 12:08:01 +0700

    Update copyTrader.ts

--- src/copyTrader.ts
+++ src/copyTrader.ts
@@ -264,7 +264,7 @@ export class CopyTrader {
         count: trades.length,
       });
 
-      for (const trade of trades) {
+            for (const trade of trades) {
         await this.handleTrade(trade);
 
         this.state.lastSeen[trader] = Math.max(
@@ -274,4 +274,5 @@ export class CopyTrader {
       }
     }),
   );
+  }
 }
